95951791e87c93df27d43902a76715bfc8f80c06
James Hollway <james.hollway@graduateinstitute.ch>
2024-10-11 14:47:45 +0200

    Added print_help() for printing a function's help page to the console

--- R/function_application.R
+++ R/function_application.R
@@ -22,3 +22,24 @@ functions_work <- function(functions, objects){
   colnames(out) <- objects
   out
 }
+
+#' @source https://www.r-bloggers.com/2013/06/printing-r-help-files-in-the-console-or-in-knitr-documents/
+#' @export
+print_help <- function(topic, format=c("text", "html", "latex", "Rd"),
+                         lines=NULL, before=NULL, after=NULL) {  
+  format=match.arg(format)
+  if (!is.character(topic)) topic <- deparse(substitute(topic))
+  helpfile = utils:::.getHelpFile(help(topic))
+  hs <- capture.output(switch(format, 
+                              text=tools:::Rd2txt(helpfile),
+                              html=tools:::Rd2HTML(helpfile),
+                              latex=tools:::Rd2latex(helpfile),
+                              Rd=tools:::prepare_Rd(helpfile)
+  )
+  )
+  if(!is.null(lines)) hs <- hs[lines]
+  hs <- c(before, hs, after)
+  cat(hs, sep="\n")
+  invisible(hs)
+}
+
